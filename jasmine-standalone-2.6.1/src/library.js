531753ac8f36fdaa5fbb10a3b0f0bbe2730b8c28
ABS User <ABS User>
2017-05-15 21:33:55 +0100

    Write getPrime function

--- jasmine-standalone-2.6.1/src/library.js
+++ jasmine-standalone-2.6.1/src/library.js
@@ -1,1 +1,42 @@
 
+function getPrime(num) 
+{
+	getPrime = (num)  => 
+	{
+		let primes = [];
+
+		if (typeof num === 'string' || num === NaN || num < 2 || typeof num === 'undefined')
+		{
+				
+			return "Invalid input" ;
+		}
+		else
+		{
+		    for (let i = 2; i <= num; i++)
+		    {
+		        if (isPrime(i))
+		        {
+		            primes.push(i);
+		        }
+		    }   
+		}
+		return primes	;
+    }
+}
+
+function isPrime (n)
+{
+	isPrime = (n) =>
+	{
+	    let squareRoot = Math.floor(Math.sqrt(n));
+
+	    for (let j = 2; j <= squareRoot; j++)
+	    {
+	        if (n % j == 0)
+	        {
+	            return false;
+	        }
+	    }
+	    return true;
+    }
+}
